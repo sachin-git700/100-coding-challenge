f6a1cf84d927058f246e719ce99c394c2ed1a39f
sachin-git700 <sachin.git700@gmail.com>
2024-07-31 09:14:22 +0530

    adds recursive approach

--- 4_fibonacci.cpp
+++ 4_fibonacci.cpp
@@ -2,26 +2,51 @@
 
 #include <iostream>
 
+void fibonacci(int a, int b, int n) {
+  std::cout << a + b << " ";
+
+  if (n <= 1) {
+    return;
+  }
+
+  fibonacci(b, a + b, --n);
+}
+
 int main() {
   int temp, a = 0, b = 1;
   int n = 10;
 
-  std::cout << a << ", ";
-  std::cout << b << ", ";
+  std::cout << a << " ";
+  std::cout << b << " ";
 
-  for (int i = 0; i < 10; i++) {
-    std::cout << a + b;
-
-    if (i != 9) {
-      std::cout << ", ";
-    }
-
-    temp = b;
-    b = a + b;
-    a = temp;
-  }
+  fibonacci(a, b, n);
 
   std::cout << std::endl;
 
   return 0;
 }
+
+// Using Iteration
+// int main() {
+//   int temp, a = 0, b = 1;
+//   int n = 10;
+
+//   std::cout << a << ", ";
+//   std::cout << b << ", ";
+
+//   for (int i = 0; i < 10; i++) {
+//     std::cout << a + b;
+
+//     if (i != 9) {
+//       std::cout << ", ";
+//     }
+
+//     temp = b;
+//     b = a + b;
+//     a = temp;
+//   }
+
+//   std::cout << std::endl;
+
+//   return 0;
+// }
